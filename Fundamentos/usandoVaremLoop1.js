4aed3a6b3caac06630fad1975eabbf3d4042ab21
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-06-22 22:07:19 -0300

    Atualizando para FOR

--- Fundamentos/usandoVaremLoop1.js
+++ Fundamentos/usandoVaremLoop1.js
@@ -1,6 +1,6 @@
-var a =2, b=0
-
-while(a>b) {
-    b++
-    console.log(b)
+for (var i=0; i<10;i++){ //var declarado no escopo global
+    console.log(i)
 }
+//for (declara algo; enquanto; faça isso) {e mais isso}
+
+console.log('i=',i)

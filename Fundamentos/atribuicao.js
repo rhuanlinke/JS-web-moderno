997f3b9c252405228db4be421c7029267f66dfa9
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-06-22 23:25:56 -0300

    Atalhos de atribuição

---
+++ Fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7 //atribuicao normal
+let b = 3
+
+b += a //atribuicao aditiva// b = b + a
+console.log(b)
+
+b -= 4 //atribuição negativa// b = b -4
+console.log(b)
+
+b *=2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b/2
+console.log(b)
+
+b %= 2 // b = b % 2
+console.log(b)

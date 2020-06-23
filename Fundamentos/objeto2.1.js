f98a4b41b071041619ae37aa3b3fc0e8ca771e9b
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-06-22 22:52:27 -0300

    Conhecendo mais de objetos

---
+++ Fundamentos/objeto2.1.js
@@ -0,0 +1,11 @@
+var i
+const objeto={
+    Testezera: "Teste",
+    ValordoTeste: 500,
+    ZicadoTeste: "F"
+}
+for(i=0;objeto.ValordoTeste>i;i++){
+    console.log(i)
+    console.log(objeto.ValordoTeste)
+}
+console.log(objeto)
---
+++ Fundamentos/objeto2.js
@@ -0,0 +1,12 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const Cliente =function(){}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class produto{} //ES 2015 (ES6) class é uma função também
+console.log(typeof produto)
+console.log(typeof new produto())
+
+//objetos são criados a partir de funções!!

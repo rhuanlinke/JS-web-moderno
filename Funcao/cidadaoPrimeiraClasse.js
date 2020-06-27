735810009e5189d2b1526299c61e92a06202d126
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-06-27 18:46:21 -0300

    Detalhando funções.
    Todas as formas de criar e utilizar funções, sendo elas>
    1. Forma literal
    2. Armazenar em variavel
    3. Armazenar em array
    4. Armazenar em atributo de objeto
    5. Passar como parâmetro
    6. Função dentro da outra

---
+++ Funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,33 @@
+// Função em JS é First-Class Object (Citizens)
+// Higher-order function
+
+// criar de forma literal
+function fun1() { }
+
+// armazenar em uma variável
+const fun2 = function /*função anônima*/ () { }
+
+// armazenar em um array
+const array = [function (a, b) {return a + b}]
+console.log(array[0](2,3))
+
+// armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function () { return 'Opa'}
+console.log(obj.falar())
+
+// passar uma função como parâmetro "fun"
+function run(fun) {
+    fun()
+}
+run(function () {console.log('Executando...')})
+
+// uma função pode retornar/conter uma função
+function soma (a,b) {
+    return function (c) {
+        console.log(a+b+c)
+    }
+}
+soma(2,3)(4)
+const cincoMais = soma(2,3)
+cincoMais(4)

ea12d3b57e62db690b3af89cfd1234dfe2036906
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-06-28 19:56:20 -0300

    This e Bind, declarando this como uma variavel para uso continuo

--- Funcao/thisEBind2.js
+++ Funcao/thisEBind2.js
@@ -1,3 +1,11 @@
-const Pessoa = () => {
+function Pessoa() {
     this.idade = 0
+
+    const self = this
+    setInterval(function(){ //executa a função uma vez a cada 1 segundo
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000) //o bind serve para pegar o idade e todas variaveis dentro da funcao pessoa (this)
 }
+
+new Pessoa

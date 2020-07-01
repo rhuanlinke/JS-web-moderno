4c05282afcc9b2099f6965c991ef2e2f8676fdb4
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-07-01 20:15:25 -0300

    Update for2.js
    Melhor entendimento e testes sobre o FOR IN
    for (indice in variavel)

--- Controle/for2.js
+++ Controle/for2.js
@@ -1,7 +1,7 @@
-//for in nos dá os atributos do elemento
+//for in nos dá a quantidade de indices do elemento nesse caso [0,1,2,3,4] (5x)
 const notas = [6, 7, 9, 8, 7]
 
-for (let i in notas) {
+for (let i in notas) { //pra cada um dos elementos da nota ele vai dar um indice i
     console.log(notas[i])
 }
 
@@ -14,4 +14,8 @@ const pessoa = {
 
 for (let atributo in pessoa) {
     console.log(atributo, '= ' + pessoa[atributo])
+}
+
+for (let e in notas) { //e é o indice, e notas a variavel/array
+    console.log("Nota nro "+e+" né msm = "+notas[e])
 }

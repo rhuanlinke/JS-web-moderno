3dc142232d410a4c9f1d4d2848607b16e6c983af
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-07-01 21:59:17 -0300

    Create iife.js
    Immediately Invoked Function Expression
    
    Geralmente utilizado para fugir do escopo global, ou o mais abrangente, definindo atributos/variaveis com const e let dentro da função somente

---
+++ Funcao/iife.js
@@ -0,0 +1,8 @@
+// IIFE -> Immediately Invoked Function Expression 
+//(Função de expressão imediatamente invocada)
+
+(function(){
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente (Normalmente Global)')
+
+})()

b9f1da50a22c9be65dd4386a86f1c9e2c1168c84
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-08-01 19:11:48 -0300

    Create oo.js
    Procedural vs OO

---
+++ Objeto/oo.js
@@ -0,0 +1,16 @@
+// CÓDIGO NÃO EXECUTÁVEL !!!
+
+// Procedural
+processamento(vlr1, vlr2, vlr3)
+
+// Orientação a Objeto
+objeto = {
+    vlr1,
+    vlr2,
+    vlr3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto

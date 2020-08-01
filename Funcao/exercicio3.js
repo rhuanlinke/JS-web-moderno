758a7960600fa581502d758d76370d6f08f751f4
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-08-01 15:07:46 -0300

    Create exercicio3.js

---
+++ Funcao/exercicio3.js
@@ -0,0 +1,15 @@
+/*Crie uma função que recebe dois parâmetros: base e expoente
+e retorne a base elevada ao expoente.*/
+var r
+
+potencia(5, 4)
+console.log(r)
+
+function potencia (b, e){
+    i = 0
+    r = b
+    for(i=0;i<e;i++){
+        r = r*b
+    }
+    return this.r
+}
